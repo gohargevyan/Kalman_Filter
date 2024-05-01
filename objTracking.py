4edd4c0c681ae0c510fc6c9d73334e01ad1326c2
Gohar Gevorgyan <114944074+gohargevyan@users.noreply.github.com>
2024-05-01 17:21:02 +0400

    Update objTracking.py

--- objTracking.py
+++ objTracking.py
@@ -1,71 +1,29 @@
-'''
-    File name         : objTracking.py
-    Description       : Main file for object tracking
-    Author            : Rahmad Sadli
-    Date created      : 20/02/2020
-    Last updated      : 20/01/2023
-    Python Version    : 3.9
-'''
-
 import cv2
-from Detector import detect
+import pyautogui  # Import the pyautogui library
 from KalmanFilter import KalmanFilter
 
 def main():
+    # Create KalmanFilter object KF
+    # KalmanFilter(dt, u_x, u_y, std_acc, x_std_meas, y_std_meas)
+    KF = KalmanFilter(0.1, 1, 1, 1, 0.1, 0.1)
 
-    # Create opencv video capture object
-    VideoCap = cv2.VideoCapture('video_randomball.avi')
+    debugMode = 1
 
-    #Variable used to control the speed of reading the video
-    ControlSpeedVar = 100  #Lowest: 1 - Highest:100
+    while True:
+        # Get cursor position
+        cursor_x, cursor_y = pyautogui.position()
 
-    HiSpeed = 100
+        # Predict
+        (x, y) = KF.predict()
 
-    #Create KalmanFilter object KF
-    #KalmanFilter(dt, u_x, u_y, std_acc, x_std_meas, y_std_meas)
+        # Update
+        (x1, y1) = KF.update((cursor_x, cursor_y))
 
-    KF = KalmanFilter(0.1, 1, 1, 1, 0.1,0.1)
-
-    debugMode=1
-
-    while(True):
-        # Read frame
-        ret, frame = VideoCap.read()
-
-        # Detect object
-        centers = detect(frame,debugMode)
-
-        # If centroids are detected then track them
-        if (len(centers) > 0):
-
-            # Draw the detected circle
-            cv2.circle(frame, (int(centers[0][0]), int(centers[0][1])), 10, (0, 191, 255), 2)
-
-            # Predict
-            (x, y) = KF.predict()
-            # Draw a rectangle as the predicted object position
-            cv2.rectangle(frame, (int(x - 15), int(y - 15)), (int(x + 15), int(y + 15)), (255, 0, 0), 2)
-
-            # Update
-            (x1, y1) = KF.update(centers[0])
-
-            # Draw a rectangle as the estimated object position
-            cv2.rectangle(frame, (int(x1 - 15), int(y1 - 15)), (int(x1 + 15), int(y1 + 15)), (0, 0, 255), 2)
-
-            cv2.putText(frame, "Estimated Position", (int(x1 + 15), int(y1 + 10)), 0, 0.5, (0, 0, 255), 2)
-            cv2.putText(frame, "Predicted Position", (int(x + 15), int(y)), 0, 0.5, (255, 0, 0), 2)
-            cv2.putText(frame, "Measured Position", (int(centers[0][0] + 15), int(centers[0][1] - 15)), 0, 0.5, (0,191,255), 2)
-
-        cv2.imshow('image', frame)
-
-        if cv2.waitKey(2) & 0xFF == ord('q'):
-            VideoCap.release()
-            cv2.destroyAllWindows()
-            break
-
-        cv2.waitKey(HiSpeed-ControlSpeedVar+1)
-
+        # Display cursor position with Kalman filter estimation
+        print("Cursor Position: ({}, {}) Estimated Position: ({}, {})".format(cursor_x, cursor_y, x1, y1))
 
 if __name__ == "__main__":
-    # execute main
+    # Execute main
     main()
+
+
